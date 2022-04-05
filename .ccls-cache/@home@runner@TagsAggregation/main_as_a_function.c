e029c688c6200b985e48e2ccea219493e0e82c1f
Ke6aP <87259657+Ke6ap@users.noreply.github.com>
2022-04-05 19:36:15 +0000

    added Levenshtein distance calculation for more accurate tree

--- .ccls-cache/@home@runner@TagsAggregation/main_as_a_function.c
+++ .ccls-cache/@home@runner@TagsAggregation/main_as_a_function.c
@@ -182,6 +182,7 @@ void printList(struct list_node* node) {
 
 /*------------------------------------END-------------------------------------- */
 
+/*------------------------ Distance calculation functions ----------------------*/
 /* Hamming distance */
 int hammingDistance(char str1[], char str2[]){
     int i = 0, count = 0;
@@ -191,10 +192,58 @@ int hammingDistance(char str1[], char str2[]){
             count++;
         i++;
     }
-      count += abs((int)strlen(str1)-(int)strlen(str2));
+    //in case of not equeal lengths
+    count += abs((int)strlen(str1)-(int)strlen(str2));
     return count;
 }
 
+/* Levenshtein distance */
+int levDistance (const char * word1,int len1,const char * word2,int len2){
+    int matrix[len1 + 1][len2 + 1];
+    int i;
+    for (i = 0; i <= len1; i++) {
+        matrix[i][0] = i;
+    }
+    for (i = 0; i <= len2; i++) {
+        matrix[0][i] = i;
+    }
+    for (i = 1; i <= len1; i++) {
+        int j;
+        char c1;
+
+        c1 = word1[i-1];
+        for (j = 1; j <= len2; j++) {
+            char c2;
+
+            c2 = word2[j-1];
+            if (c1 == c2) {
+                matrix[i][j] = matrix[i-1][j-1];
+            }
+            else {
+                int delete;
+                int insert;
+                int substitute;
+                int minimum;
+
+                delete = matrix[i-1][j] + 1;
+                insert = matrix[i][j-1] + 1;
+                substitute = matrix[i-1][j-1] + 1;
+                minimum = delete;
+                if (insert < minimum) {
+                    minimum = insert;
+                }
+                if (substitute < minimum) {
+                    minimum = substitute;
+                }
+                matrix[i][j] = minimum;
+            }
+        }
+    }
+    return matrix[len1][len2];
+}
+/*-----------------------------------------------------------------------------*/
+
+/* tags combination */
 char *combination(char *str_1, char *str_2){
   char *final_str;
   final_str = (char*)malloc ((strlen(str_1)+strlen(str_2)+1) * sizeof (char));
@@ -332,7 +381,11 @@ struct node* not_main(int argc, char **argv) {
 
   for (int i =0; i < argc-1; i++){
     for(int j=i+1;j<argc-1;++j){
-      differences[i][j] = hammingDistance(tags_table[i],tags_table[j]);
+      //hamming
+      //differences[i][j] = hammingDistance(tags_table[i],tags_table[j]);
+      //lev
+      differences[i][j] = levDistance(tags_table[i],strlen(tags_table[i]),tags_table[j],strlen(tags_table[j]));
+      
     }
   }
 
@@ -483,7 +536,11 @@ struct node* not_main(int argc, char **argv) {
       j = i+1;
       while(temp_2 != NULL){
         //printf("\ncompared to -->id:%d tags:%s:\n",temp_2->id,temp_2->tags);
-        differences[i][j] = hammingDistance(temp->tags,temp_2->tags);
+        //hamming
+        //differences[i][j] = hammingDistance(temp->tags,temp_2->tags);
+        //lev
+        differences[i][j] = levDistance(temp->tags,strlen(temp->tags),temp_2->tags,strlen(temp_2->tags));
+        
         //printf("%s   %s\n",temp->tags,temp_2->tags);
 
         temp_2 = temp_2->next;
--- main.c
+++ main.c
@@ -181,6 +181,7 @@ void printList(struct list_node* node) {
 }
 
 /*------------------------------------END-------------------------------------- */
+/*------------------------ Distance calculation functions ----------------------*/
 
 /* Hamming distance */
 int hammingDistance(char str1[], char str2[]){
@@ -196,6 +197,54 @@ int hammingDistance(char str1[], char str2[]){
     return count;
 }
 
+/* Levenshtein distance */
+int levDistance (const char * word1,int len1,const char * word2,int len2){
+    int matrix[len1 + 1][len2 + 1];
+    int i;
+    for (i = 0; i <= len1; i++) {
+        matrix[i][0] = i;
+    }
+    for (i = 0; i <= len2; i++) {
+        matrix[0][i] = i;
+    }
+    for (i = 1; i <= len1; i++) {
+        int j;
+        char c1;
+
+        c1 = word1[i-1];
+        for (j = 1; j <= len2; j++) {
+            char c2;
+
+            c2 = word2[j-1];
+            if (c1 == c2) {
+                matrix[i][j] = matrix[i-1][j-1];
+            }
+            else {
+                int delete;
+                int insert;
+                int substitute;
+                int minimum;
+
+                delete = matrix[i-1][j] + 1;
+                insert = matrix[i][j-1] + 1;
+                substitute = matrix[i-1][j-1] + 1;
+                minimum = delete;
+                if (insert < minimum) {
+                    minimum = insert;
+                }
+                if (substitute < minimum) {
+                    minimum = substitute;
+                }
+                matrix[i][j] = minimum;
+            }
+        }
+    }
+    return matrix[len1][len2];
+}
+
+/*-----------------------------------------------------------------------------*/
+
+/* tags combination */
 char *combination(char *str_1, char *str_2){
   char *final_str;
   final_str = (char*)malloc ((strlen(str_1)+strlen(str_2)+1) * sizeof (char));
@@ -347,7 +396,10 @@ int main(int argc, char **argv) {
 
   for (int i =0; i < argc-1; i++){
     for(int j=i+1;j<argc-1;++j){
-      differences[i][j] = hammingDistance(tags_table[i],tags_table[j]);
+      //hamming
+      //differences[i][j] = hammingDistance(tags_table[i],tags_table[j]);
+      //lev
+      differences[i][j] = levDistance(tags_table[i],strlen(tags_table[i]),tags_table[j],strlen(tags_table[j]));
     }
   }
 
@@ -498,7 +550,10 @@ int main(int argc, char **argv) {
       j = i+1;
       while(temp_2 != NULL){
         //printf("\ncompared to -->id:%d tags:%s:\n",temp_2->id,temp_2->tags);
+        //hamming
         differences[i][j] = hammingDistance(temp->tags,temp_2->tags);
+        //lev
+        differences[i][j] = levDistance(temp->tags,strlen(temp->tags),temp_2->tags,strlen(temp_2->tags));
         //printf("%s   %s\n",temp->tags,temp_2->tags);
 
         temp_2 = temp_2->next;
